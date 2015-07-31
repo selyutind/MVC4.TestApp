d1aa117dbffbda0a425e158d050677482a44dfc4
Immortall08 <punleon@gmail.com>
2015-07-31 17:44:47 +0300

    refactoring

---
+++ TestApp.Web/App/Main/facrotys/redirectUrl.factory.js
@@ -0,0 +1,26 @@
+﻿(function () {
+	'use strict';
+
+	angular
+        .module('app')
+        .factory('redirectUrl', redirectUrl);
+	redirectUrl.$inject =['$window'];
+
+	function redirectUrl($window) {
+		var redirectUrl = {};
+		redirectUrl.users = function () {
+			var url = "#/users";
+			$window.location.href = url;
+		}
+		redirectUrl.createUser = function () {
+			var url = "#/createUser";
+			$window.location.href = url;
+		}
+		redirectUrl.editUser = function () {
+			var url = "#/editUser";
+			$window.location.href = url;
+		}
+		return redirectUrl;
+	}
+
+})();
